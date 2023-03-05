9bab327415b0d161ef856d8c3b5fe96dde4c483b
cindymuthoni <cindymuthoni14@gmail.com>
2023-03-05 17:30:43 +0300

    completed

---
+++ Development/phase3/pet-finder/src/App.js
@@ -0,0 +1,32 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import React from "react"
+import Home from "./Components/Home"
+import SingleDog from "./Components/HomePage"
+import LoginPage from "./Components/LoginPage"
+import RegisterPage from "./Components/RegisterPage"
+import HomePage from "./Components/HomePage"
+import LandingPage from "./Components/LandingPage"
+import ForgetPasswordPage from "./Components/ForgetPasswordPage"
+// import './index.css'
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={< RegisterPage />} />
+          <Route path="/forget-password" element={<ForgetPasswordPage />} />
+          <Route path="/:name" element={<SingleDog />}></Route>
+          <Route path="/home" element={< HomePage />} />
+          <Route path="/" element={<LandingPage />} />
+          "
+        </Routes>
+        </div>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ Development/phase3/pet-finder/src/Components/LoginPage.js
@@ -0,0 +1,38 @@
+
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+//import '../App.css'
+export default function SignInPage() {
+    // let 
+    let [login, setLogin] = useState(null)
+    return (
+        <div className="text-center login-body m-5 auto">
+            <div className="login-form">
+                <h2 className="title-sign">Sign in </h2>
+                <form className="form" action="/home">
+                    <p className="paragraph">
+                        <label className="labels">Username or email address</label><br />
+                        <input type="text" name="first_name" required onChange={(e => {
+                            setLogin({ ...login, username: e.target.value })
+                        })} />
+                    </p>
+                    <p className="paragraph">
+                        <label ClassName="labels">Password</label>
+                        <Link to="/forget-password"><label className="right-label">Forget password?</label></Link>
+                        <br />
+                        <input type="password" name="password" required onChange={e => {
+                            setLogin({...login, password:e.target.value})
+                        }}/>
+                    </p>
+                    <p className="paragraph">
+                        <button id="sub_btn" type="submit">Login</button>
+                    </p>
+                </form>
+                <footer >
+                    <p className="paragraph">First time? <Link to="/register">Create an account</Link>.</p>
+                    <p><Link to="/">Back to Homepage</Link>.</p>
+                </footer>
+            </div>
+        </div>
+    )
+}
